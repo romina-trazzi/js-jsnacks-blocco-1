13848c5442a8585a53d79c1d58e69e25569ba659
Romina Trazzi <romina.trazzi@posta.it>
2021-01-03 01:57:30 +0100

    Snack 5-2 (funzionante)

--- snack5-2/main.js
+++ snack5-2/main.js
@@ -2,12 +2,23 @@
     =            CODICE SNACK 5-2            =
 =============================================*/
 
-var stringaRight = [prompt("Inserisci una parola da rovesciare")];
+var stringaRight = prompt("Inserisci una parola da rovesciare");
+var reverse;
 
-function isReverse (stringaRight) {
-    return stringaRight.split("").reverse().join("");
+function isReverse(parola) { 
+    var reverse = parola.split("").reverse().join("");
+    return reverse;
 }
 
-console.log(isReverse(stringaRight));
+var markup = 
+
+   ` <div class = "parola_rovescia">
+        <h2> ${stringaRight} diventa ` + isReverse(stringaRight) + `</h2>
+    </div> `
+    
+document.getElementById("prova").insertAdjacentHTML("beforeend", markup)
+
+
+
 
 
